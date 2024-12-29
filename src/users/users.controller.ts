1875b2caceb4d79bb47c7937513cf2ade6056978
Muhammad Hashir Malik <m.hashir.malik1@gmail.com>
2024-12-29 14:35:54 +0500

    route/api/user/delete
    1. This route is used to delete the user.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,9 @@
 import { UsersService } from './users.service';
 import { AuthenticatedRequest } from 'src/auth/auth.interface';
 import { UpdateUsernameDto } from './dto/update-user-name.dto';
-import { Controller, HttpCode, HttpStatus, Get, Req, Body, Patch } from '@nestjs/common';
+import { Controller, HttpCode, HttpStatus, Get, Req, Body, Patch, Delete } from '@nestjs/common';
 
-@Controller('users')
+@Controller('user')
 export class UsersController {
   
   constructor(private readonly usersService: UsersService) {}
@@ -18,5 +18,11 @@ export class UsersController {
   @HttpCode(HttpStatus.NO_CONTENT)
   async updateName(@Req() request: AuthenticatedRequest, @Body() body: UpdateUsernameDto) {
     await this.usersService.updateName(request, body);
+  }
+
+  @Delete('delete')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async deleteUser(@Req() request: AuthenticatedRequest) {
+    await this.usersService.deleteUser(request);
   }
 }
