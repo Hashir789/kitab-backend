ba10f38cc56cfc3b66f33ccac3ba5afa7fdeec63
Muhammad Hashir Malik <m.hashir.malik1@gmail.com>
2025-01-16 23:37:23 +0500

    route/api/item/hide
    1. This route is used to hide an item.

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,6 +1,7 @@
 import { ItemsService } from './items.service';
 import { ItemCreateDto } from './dto/item-create.dto';
-import { Controller, HttpCode, HttpStatus, Post, Body } from '@nestjs/common';
+import { Controller, HttpCode, HttpStatus, Post, Body, Patch } from '@nestjs/common';
+import { ItemHideDto } from './dto/item-hide.dto';
 
 @Controller('item')
 export class ItemsController {
@@ -11,6 +12,12 @@ export class ItemsController {
   @HttpCode(HttpStatus.CREATED)
   async createItem(@Body() body: ItemCreateDto) {
     return this.itemsService.createItem(body);
+  }
+
+  @Patch('hide')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async hideItem(@Body() body: ItemHideDto) {
+    await this.itemsService.hideItem(body);
   }
 
 }
