d263bc7471ab49403e3d3b269a164eefa89b4092
Muhammad Hashir Malik <m.hashir.malik1@gmail.com>
2025-01-22 00:16:55 +0500

    route/api/record/update
    1. This route is used to update any given record.

--- src/record/record.service.ts
+++ src/record/record.service.ts
@@ -2,8 +2,9 @@ import { format } from 'date-fns';
 import { Logger } from 'src/logger/logger.service';
 import { ReadRecordDto } from './dto/record-read.dto';
 import { CreateRecordDto } from './dto/record-create.dto';
+import { updateRecordDto } from './dto/record-update.dto';
 import { PostgresService } from 'src/postgres/postgres.service';
-import { Injectable, HttpException, BadRequestException } from '@nestjs/common';
+import { Injectable, HttpException, BadRequestException, NotFoundException } from '@nestjs/common';
 
 @Injectable()
 export class RecordsService {
@@ -27,12 +28,23 @@ export class RecordsService {
     }
   }
 
-  async createRecord(query: CreateRecordDto): Promise<{ id: number, item_id: number, scale_id: number, count: number, date: Date }> {
+  async createRecord(body: CreateRecordDto): Promise<{ id: number, item_id: number, scale_id: number, count: number, date: Date }> {
     try {
-      const { item_id, scale_id, count, date } = query;
+      const { item_id, scale_id, count, date } = body;
       this.loggerService.log('createRecord {controller}');
       const record = await this.createRecordQuery(item_id, scale_id, count, date);
       return record;
+    } catch(error) {
+      this.loggerService.error(error.message, error.status ?? 500);
+      throw new HttpException(error.message, error.status ?? 500);
+    }
+  }
+
+  async updateRecord(body: updateRecordDto): Promise<void> {
+    try {
+      const { id, date } = body;
+      this.loggerService.log('updateRecord {controller}');
+      await this.updateRecordQuery(id , date);
     } catch(error) {
       this.loggerService.error(error.message, error.status ?? 500);
       throw new HttpException(error.message, error.status ?? 500);
@@ -67,5 +79,21 @@ export class RecordsService {
       throw new BadRequestException('Invalid email or credentials');
     }
     return result[0];
+  }
+
+  async updateRecordQuery(id: number, date: Date | null): Promise<void> {
+    this.loggerService.log('updateRecordQuery {query}');
+    const formattedDate = format(date, 'yyyy-MM-dd 00:00:00');
+    const result: { id: number }[] = await this.postgresService.query(`
+      UPDATE records
+      SET
+        date = COALESCE($2, date)
+      WHERE id = $1
+      RETURNING id;
+      `,
+      [id, formattedDate]
+    );
+    if (!result.length)
+      throw new NotFoundException('Record not found');
   }
 }
